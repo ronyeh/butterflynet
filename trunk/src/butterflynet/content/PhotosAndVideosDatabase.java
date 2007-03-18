14f56ceda7a24d3177b666f9b912d8d4b05dd1b1
ronyeh <ronyeh@ecbca8fb-2922-0410-aedb-db38ad70d144>
2007-03-18 04:51:31 +0000

    Simple support for video using JMF/FMJ... but I can't get it working the way I want (export the first frame). =\

--- trunk/src/butterflynet/content/PhotosAndVideosDatabase.java
+++ trunk/src/butterflynet/content/PhotosAndVideosDatabase.java
@@ -3,6 +3,7 @@ package butterflynet.content;
 import java.io.File;
 import java.util.List;
 
+import net.sf.fmj.ui.application.PlayerPanel;
 import edu.stanford.hci.r3.util.DebugUtils;
 import edu.stanford.hci.r3.util.files.FileUtils;
 import edu.stanford.hci.r3.util.files.SortDirection;
@@ -19,9 +20,9 @@ import edu.stanford.hci.r3.util.files.SortDirection;
  * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
  * 
  */
-public class PhotosDatabase {
+public class PhotosAndVideosDatabase {
 
-	public PhotosDatabase(File photosPath, File settingsPath) {
+	public PhotosAndVideosDatabase(File photosPath, File settingsPath) {
 		// find all files in docsPath
 		List<File> files = FileUtils.listVisibleFilesRecursively(photosPath);
 		// DebugUtils.println(files);
@@ -34,11 +35,36 @@ public class PhotosDatabase {
 		// DebugUtils.println(f);
 		// }
 		// SystemUtils.toc();
-		
-		
+
 		// check the settings to see what was the last file we had processed...
 		File newestPhoto = files.get(0);
 		DebugUtils.println("Newest photo is: " + newestPhoto);
+
+		createPhotoThumbnails(files);
 	}
 
+	/**
+	 * Seems like a reasonable list of thumbnail sizes (pixels on the longer side) is... 100 (like
+	 * flickr), 128 pixels (close to facebook's), 256 wide (close to flickr's small).
+	 * 
+	 * @param files
+	 */
+	private void createPhotoThumbnails(List<File> files) {
+
+		for (File f : files) {
+			if (f.getName().toLowerCase().endsWith(".avi")) {
+				DebugUtils.println(f);
+			}
+		}
+
+		// Failed Attempt at processing video...
+		// Use FMJ instead of JMF (or JVLC)...
+		// file://C:\Documents and Settings\Ron Yeh\My Documents\Projects\ButterflyNet2\MVI_1769.AVI
+		// String movie = "../Data/BNet2Data/Photos & Videos/Ron's/March 22, 2005/MVI_1777.AVI";
+		// File movieFile = new File(movie);
+		// PlayerPanel.main(new String[] {});
+		
+		
+		// create a "unique" thumbnail name based on the current file name,
+	}
 }
